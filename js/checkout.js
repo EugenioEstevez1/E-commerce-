1f166742c7fbdc8acfd485ba21ee41dd977b3731
Eugenio <eugenioestevez15@gmail.com>
2023-11-28 17:57:48 -0300

    Tienda de mate

--- js/checkout.js
+++ js/checkout.js
@@ -1,8 +1,8 @@
 const tbody = document.querySelector('tbody')
 const list = document.querySelector("#eliminar");
 
-function tablaHTML(productoCarrito){
-    return `<tr>
+function tablaHTML(productoCarrito) {
+  return `<tr>
     <td>${productoCarrito.codigo}</td>
     <td>${productoCarrito.nombre}</td>
     <td>${productoCarrito.importe}</td>
@@ -12,50 +12,49 @@ function tablaHTML(productoCarrito){
     </tr>`
 }
 
-if (carritoProductos.length > 0){
-    carritoProductos.forEach(productoCarrito => {
-        tbody.innerHTML += tablaHTML(productoCarrito)
-    })
+if (carritoProductos.length > 0) {
+  carritoProductos.forEach(productoCarrito => {
+    tbody.innerHTML += tablaHTML(productoCarrito)
+  })
 }
 
 document.querySelector("#clave").addEventListener("click", (e) => {
-    target = e.target;
-    target.classList.contains("delete") ? target.parentElement.parentElement.remove() : null
-  
-  });
-  
-  function eliminarElementoCarrito(clave) {
-    if (typeof Storage !== "undefined") {
-      let carrito = JSON.parse(localStorage.getItem("miCarrito")) || [];
-      let indice = carrito.findIndex(item => item.clave === clave);
-      indice !== -1 ? (carrito.splice(indice, 1), localStorage.setItem("miCarrito", JSON.stringify(carrito))) : null;
-    }
-  
+  target = e.target;
+  target.classList.contains("delete") ? target.parentElement.parentElement.remove() : null
+
+});
+
+function eliminarElementoCarrito(clave) {
+  if (typeof Storage !== "undefined") {
+    let carrito = JSON.parse(localStorage.getItem("miCarrito")) || [];
+    let indice = carrito.findIndex(item => item.clave === clave);
+    indice !== -1 ? (carrito.splice(indice, 1), localStorage.setItem("miCarrito", JSON.stringify(carrito))) : null;
   }
-  
-  document.querySelector("#clave").addEventListener("click", (e) => {
-    let target = e.target;
-    if (target.classList.contains("delete")) {
-      let clave = target.parentElement.parentElement.dataset.clave;
-      eliminarElementoCarrito(clave);
-      mensajeToast()
-  
-  
-    }
-  });
-  
-  function mensajeToast() {
-    Toastify({
-      text: `Se eliminó el producto del carrito.`,
-      duration: 1500,
-      close: true,
-      gravity: "bottom",
-      position: "right",
-      stopOnFocus: true,
-      style: {
-        background: "red",
-        color: "white",
-      }
-    }).showToast();
+
+}
+
+document.querySelector("#clave").addEventListener("click", (e) => {
+  let target = e.target;
+  if (target.classList.contains("delete")) {
+    let clave = target.parentElement.parentElement.dataset.clave;
+    eliminarElementoCarrito(clave);
+    mensajeToast()
+
+
   }
-  
+});
+
+function mensajeToast() {
+  Toastify({
+    text: `Se eliminó el producto del carrito.`,
+    duration: 1500,
+    close: true,
+    gravity: "bottom",
+    position: "right",
+    stopOnFocus: true,
+    style: {
+      background: "red",
+      color: "white",
+    }
+  }).showToast();
+}
